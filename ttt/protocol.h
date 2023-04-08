a7df1f6481db87d125ab403aed2605b137048238
Alphan Yang <90988780+alphanyang@users.noreply.github.com>
2023-04-07 21:28:19 -0400

    ...

--- ttt/protocol.h
+++ ttt/protocol.h
@@ -29,8 +29,23 @@ void send_message(int sockfd, const char *msg);
 char *receive_message(int sockfd);
 Message *parse_message(const char *msg);
 char *format_message(const char *msg_type, ...);
+
+
+char *game_state_string();
+void update_board(char *board, const char *move, char *role);
+int process_move(char player_role, int row, int col);
+const char *get_board();
 int validate_move(const char *move);
-int game_over(const char *board);
-void update_board(char *board, const char *move, const char *role);
+char game_over(const char *board);
+
+void lock_game_state();
+void unlock_game_state();
+void reset_game_state();
+
+char get_current_player();
+void set_player_socket(char role, int socket);
+void set_current_player(char role);
+int get_player_socket(char role);
+
 
 #endif // PROTOCOL_H
